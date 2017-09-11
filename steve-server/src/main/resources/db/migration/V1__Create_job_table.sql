e5b4a0619d09fbd105a5c20ec77db6c54ab9455c
GP <ganeshp@indix.com>
2017-09-11 18:06:02 +0530

    Set default now() for created_at field.

--- steve-server/src/main/resources/db/migration/V1__Create_job_table.sql
+++ steve-server/src/main/resources/db/migration/V1__Create_job_table.sql
@@ -3,6 +3,6 @@ create table "job" (
   "app_name" TEXT NOT NULL,
   "status" TEXT NOT NULL,
   "attributes" JSONB NOT NULL,
-  created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-  updated_at TIMESTAMP WITHOUT TIME ZONE NULL
+  created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT now(),
+  updated_at TIMESTAMP WITHOUT TIME ZONE NULL DEFAULT NULL
 );
