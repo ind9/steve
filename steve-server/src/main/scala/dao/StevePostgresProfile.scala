7320426698c57a126a13b64509e7f18b20610a14
GP <ganeshp@indix.com>
2017-09-12 12:14:55 +0530

    No generic names.

--- steve-server/src/main/scala/dao/StevePostgresProfile.scala
+++ steve-server/src/main/scala/dao/StevePostgresProfile.scala
@@ -15,15 +15,15 @@ trait StevePostgresProfile extends ExPostgresProfile
   override protected def computeCapabilities: Set[Capability] =
     super.computeCapabilities + JdbcCapabilities.insertOrUpdate
 
-  override val api = MyAPI
+  override val api = StevePostgresAPI
 
-  object MyAPI extends API with ArrayImplicits
+  object StevePostgresAPI extends API with ArrayImplicits
     with DateTimeImplicits
     with JsonImplicits {
     implicit val strListTypeMapper = new SimpleArrayJdbcType[String]("text").to(_.toList)
     implicit val playJsonArrayTypeMapper =
       new AdvancedArrayJdbcType[JsValue](pgjson,
-        (s) => utils.SimpleArrayUtils.fromString[JsValue](Json.parse(_))(s).orNull,
+        (s) => utils.SimpleArrayUtils.fromString[JsValue](Json.parse)(s).orNull,
         (v) => utils.SimpleArrayUtils.mkString[JsValue](_.toString())(v)
       ).to(_.toList)
     implicit val JavaUtilDateMapper =
