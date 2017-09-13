952a4cc1af6a5dd396becf3ce96a55fb3fe3b761
GP <ganeshp@indix.com>
2017-09-13 15:07:05 +0530

    Finish up the remaining Job client methods and their tests.

--- steve-client-scala/src/main/scala/steve/client/SteveClient.scala
+++ steve-client-scala/src/main/scala/steve/client/SteveClient.scala
@@ -11,7 +11,7 @@ import scalaj.http.BaseHttp
 class SteveClient(httpClient: BaseHttp, host: String) {
   def addJob(job: Job): Option[String] = {
     val data = JsonUtils.toJson(job)
-    val response = httpClient(host + "/job")
+    val response = httpClient(s"${host}/job")
       .postData(data)
       .method("PUT")
       .asString
@@ -20,10 +20,28 @@ class SteveClient(httpClient: BaseHttp, host: String) {
   }
 
   def getJob(jobId: UUID): Job = {
-    val response = httpClient(host + "/job")
-          .method("GET")
-          .asString
+    val response = httpClient(s"${host}/job/${jobId.toString}")
+      .method("GET")
+      .asString
     val jobInfo = JsonUtils.fromJson[Job](response.body)
     jobInfo
+  }
+
+  def updateJob(jobId: UUID, updatedJob: Job): Job = {
+    val data = JsonUtils.toJson(updatedJob)
+    val response = httpClient(s"${host}/job/${jobId.toString}")
+      .postData(data)
+      .method("POST")
+      .asString
+    val jobInfo = JsonUtils.fromJson[Job](response.body)
+    jobInfo
+  }
+
+  def deleteJob(jobId: UUID): Option[String] = {
+    val response = httpClient(s"${host}/job/${jobId.toString}")
+      .method("DELETE")
+      .asString
+    val jobInfo = JsonUtils.fromJson[Map[String, String]](response.body)
+    jobInfo.get("rowsAffected")
   }
 }
--- steve-client-scala/src/test/scala/steve/client/SteveClientSpec.scala
+++ steve-client-scala/src/test/scala/steve/client/SteveClientSpec.scala
@@ -11,7 +11,7 @@ import utils.JsonUtils
 import scalaj.http.{BaseHttp, HttpResponse}
 
 class SteveClientSpec extends FlatSpec {
-  val host = "http://localhost:9092"
+  val host = "http://localhost:9092" // TODO - move this to config?
   val mockHttp = mock(classOf[BaseHttp], RETURNS_DEEP_STUBS)
   val client = new SteveClient(mockHttp, host)
 
@@ -22,7 +22,7 @@ class SteveClientSpec extends FlatSpec {
     val job = Job(id = jobId, appName = "cannonball", state = "START", createdAt = new Date(), updatedAt = None, attributes = Map("test" -> "test"))
 
     when {
-      mockHttp(host + "/job")
+      mockHttp(s"${host}/job")
         .postData(JsonUtils.toJson(job))
         .method("PUT")
         .asString
@@ -35,15 +35,46 @@ class SteveClientSpec extends FlatSpec {
   "Client" should "get a Job's details for a given Job ID" in {
     val jobId = UUID.randomUUID
     val dummyJob = Job(id = jobId, appName = "cannonball", state = "START", createdAt = new Date(), updatedAt = None, attributes = Map("test" -> "test"))
-    val expectedResponse = JsonUtils.toJson(dummyJob)
+    val dummyResponse = JsonUtils.toJson(dummyJob)
 
     when {
-      mockHttp(host + "/job")
+      mockHttp(s"${host}/job/${jobId.toString}")
         .method("GET")
         .asString
-    }.thenReturn(HttpResponse[String](expectedResponse, 201, Map()))
+    }.thenReturn(HttpResponse[String](dummyResponse, 200, Map()))
 
     val response = client.getJob(jobId)
     response should be(dummyJob)
+  }
+
+  "Client" should "update a Job's details" in {
+    val jobId = UUID.randomUUID
+    val dummyJob = Job(id = jobId, appName = "cannonball", state = "UPDATING", createdAt = new Date(), updatedAt = None, attributes = Map("test" -> "test"))
+    val dummyResponse = JsonUtils.toJson(dummyJob)
+
+    when {
+      mockHttp(s"${host}/job/${jobId.toString}")
+        .postData(JsonUtils.toJson(dummyJob))
+        .method("POST")
+        .asString
+    }.thenReturn(HttpResponse[String](dummyResponse, 200, Map()))
+
+    val response = client.updateJob(jobId, dummyJob)
+    response should be(dummyJob)
+  }
+
+  "Client" should "get a delete a Job, given a Job ID" in {
+    val jobId = UUID.randomUUID
+    val expectedResponse = Map("id" -> jobId.toString, "msg" -> "Deleted", "rowsAffected" -> "1")
+    val jsonResponse = JsonUtils.toJson(expectedResponse)
+
+    when {
+      mockHttp(s"${host}/job/${jobId.toString}")
+        .method("DELETE")
+        .asString
+    }.thenReturn(HttpResponse[String](jsonResponse, 200, Map()))
+
+    val response = client.deleteJob(jobId)
+    response should be(expectedResponse.get("rowsAffected"))
   }
 }
