fcecf5e231160623854c2f012355e3d63b5383ac
GP <ganeshp@indix.com>
2017-09-18 13:51:37 +0530

    Stats DB method.

--- steve-server/src/main/scala/dao/Entity.scala
+++ steve-server/src/main/scala/dao/Entity.scala
@@ -4,16 +4,21 @@ import java.util.{Date, UUID}
 
 import com.google.inject.{Inject, Singleton}
 import dao.StevePostgresProfile.api._
-import domain.{Job,Item}
+import domain.{Item, Job}
 
 import scala.concurrent.Future
 
 class JobTable(tag: Tag) extends Table[Job](tag, Some("public"), "job") {
   def id = column[UUID]("id", O.PrimaryKey)
+
   def appName = column[String]("app_name")
+
   def state = column[String]("state")
+
   def createdAt = column[Date]("created_at", O.Default(new Date()), O.SqlType("timestamp not null default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP"))
+
   def updatedAt = column[Option[Date]]("updated_at")
+
   def attributes = column[Map[String, String]]("attributes")
 
   def * = (id, appName, state, createdAt, updatedAt, attributes) <> (Job.tupled, Job.unapply)
@@ -21,13 +26,19 @@ class JobTable(tag: Tag) extends Table[Job](tag, Some("public"), "job") {
 
 class ItemTable(tag: Tag) extends Table[Item](tag, Some("public"), "item") {
   def id = column[UUID]("id", O.PrimaryKey)
+
   def jobId = column[UUID]("job_id")
+
   def status = column[String]("status")
+
   def createdAt = column[Date]("created_at", O.Default(new Date()), O.SqlType("timestamp not null default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP"))
+
   def updatedAt = column[Option[Date]]("updated_at")
+
   def attributes = column[Map[String, String]]("attributes")
 
   protected val jobTableQuery = TableQuery[JobTable]
+
   def jobForeignKey = foreignKey("JOB_ID_FK", jobId, jobTableQuery)(_.id, onDelete=ForeignKeyAction.Cascade)
 
   def * = (id, jobId, status, createdAt, updatedAt, attributes) <> (Item.tupled, Item.unapply)
@@ -38,7 +49,7 @@ class ItemTable(tag: Tag) extends Table[Item](tag, Some("public"), "item") {
 //}
 
 //abstract class AbstractDAO {
- // val db = Database.forConfig("steveDatasource")
+// val db = Database.forConfig("steveDatasource")
 //}
 
 @Singleton
@@ -95,6 +106,19 @@ class Items @Inject()(db: Database) extends TableQuery(new ItemTable(_)) {
   def delete(itemId: UUID): Future[Int] = {
     db.run {
       this.filter(_.id === itemId).delete
+    }
+  }
+
+  // TODO refactor and move this to a nicer place.
+  def stats(jobId: UUID): Future[_] = {
+    db.run {
+      this.filter(_.jobId === jobId)
+        .groupBy(_.status)
+        .map {
+          case (id, group) => (id, group.map(_.status).length)
+        }
+        .to[List]
+        .result
     }
   }
 }
