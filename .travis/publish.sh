985981496cead3b6a37ce504e580381d2c108ebe
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2017-09-22 16:31:28 +0530

    Fixing the deploy script path + making publish.sh executable

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -1,4 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+set -x
+
 sbt +publishSigned         #This will sign and upload your artifact to Sonatype’s staging repository.
 sbt sonatypeRelease      #This promotes the release to be ready for syncing to Maven Central.
+
 echo "Released"
