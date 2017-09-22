b3fb18b4b19b7d2f220a728b908a90fe85ba45c2
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2017-09-22 16:35:54 +0530

    Error out if something fails than going ahead like nothing happened

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -x
+set -x -e
 
 sbt +publishSigned         #This will sign and upload your artifact to Sonatype’s staging repository.
 sbt sonatypeRelease      #This promotes the release to be ready for syncing to Maven Central.
